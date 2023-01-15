twoerner/u-boot__smaeul

riscv: Add BL808 board
Signed-off-by: Samuel Holland <samuel@sholland.org>

include/configs/bl808.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_SYS_INIT_RAM_ADDR	0x22020000
+#define CONFIG_SYS_INIT_RAM_SIZE	0x00034000 // see GLB_Set_EM_Sel()
+
+#endif /* __CONFIG_H */

vladimir-dudnik-1/u-boot

include/configs/bl808.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CFG_SYS_INIT_RAM_ADDR	0x22020000
+#define CFG_SYS_INIT_RAM_SIZE	0x00034000 // see GLB_Set_EM_Sel()
+
+#endif /* __CONFIG_H */